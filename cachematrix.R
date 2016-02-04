e9f7bcec003d85c2f713c3fb8e153b7aadbe3273
ansarim <ansarim@gmail.com>
2016-02-04 15:05:20 -0500

    Committing changes to cachematrix.R that does caching of inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This set of 2 functions creates and caches inverse of a inversiable
+# matrix.
 
-## Write a short comment describing this function
+# makeCacheMatrix() caches the inverse in the parent environment
+# User can also clear the cache, get the matrix or set the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(y = matrix()) {
+    mi <- NULL
+    set <- function(x) {
+        y <<- x
+        mi <<- NULL
+    }
+    get <- function() y
+    setinverse <- function(inverse) mi <<- inverse
+    getinverse <- function() mi
+    list(set = set, get = get, setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+# cacheSolve() takes the list object that contains methods to
+# set/get/setinverse/getinverse.
 
-## Write a short comment describing this function
+# if the inverse matrix is already cached, it will get it from 
+# the cache. If not, it will calculate the inverse and stores
+# in the variable mi.
+# Variable mi is in the environment of where cacheSolve()
+# is being called but, is set in the environemt of makeCacheMatrix()
+# using <<- operation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mi <- x$getinverse()
+    if(!is.null(mi)) {
+        message("getting cached inverse matrix")
+        return(mi)
+    }
+    data <- x$get()
+    mi <- solve(data, ...)
+    x$setinverse(mi)
+    mi
+    
 }
